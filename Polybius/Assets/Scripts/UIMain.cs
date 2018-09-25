1a10083cfc5cf43a9f9e5f0bfe7063b639163fec
tr-io <leoliu.mailbox@gmail.com>
2018-09-24 23:02:05 -0400

    Added GUI elements and Register/Login UIs

--- Polybius/Assets/Scripts/UIMain.cs
+++ Polybius/Assets/Scripts/UIMain.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIMain : MonoBehaviour {
+public class UIMain : MonoBehaviour
+{
+    public List<GameObject> UIPanels;
+    public GameObject initialPanel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private GameObject activePanel;
+
+    // always opens the initial panel so runtime will always look good
+    // makes it so it doesn't matter what panel we have active while editing
+    private void Start()
+    {
+        // TODO: Change logic so we take playerprefs and know when they logged in or not
+        foreach (GameObject g in UIPanels)
+        {
+            g.SetActive(false);
+        }
+
+        initialPanel.SetActive(true);
+        activePanel = initialPanel;
+    }
+
+    // method to switch panels
+    public void ChangeMenu(GameObject panel)
+    {
+        activePanel.SetActive(false);
+        panel.SetActive(true);
+
+        activePanel = panel;
+    }
 }
