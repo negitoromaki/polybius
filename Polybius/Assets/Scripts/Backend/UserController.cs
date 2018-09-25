49b6395dee2039d9b918142a293aa2fc0eab79a2
tr-io <leoliu.mailbox@gmail.com>
2018-09-25 10:51:42 -0400

    Added UserController singleton
    Some folder reorganization
    Created a prefab out of menu GUI
    Added UserController script which will control user authentication and pipeline to the smartfox server

---
+++ Polybius/Assets/Scripts/Backend/UserController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Script that handles User authentication, retrieving values from the database,
+ * storing statistics in the database, etc.
+ * Acts as a singleton between all scenes
+ * Only can have one instance at a time
+ */
+
+public class UserController : MonoBehaviour
+{
+
+}
