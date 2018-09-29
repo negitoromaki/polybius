b91ed1657095e7cb9dbb82ec73ce5a814916c045
csculley <cs.sculley@gmail.com>
2018-09-28 23:28:21 -0400

    Create polybiusmanager and messaging scripts

---
+++ Polybius/Assets/Scripts/UI/MessageScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace polybius {
+    public class MessageScript : MonoBehaviour {
+
+        // References
+        public GameObject greenMessage, blueMessage;
+        private GameObject messageParent;
+
+        private void Start() {
+            messageParent = this.transform.Find("Messages").gameObject;
+        }
+
+        void Awake() {
+            PolybiusManager.getMessages();
+            /*
+            for (int i = 0; i < PolybiusManager.messages.Count; i++) {
+
+                // Create message
+                GameObject message;
+                if (PolybiusManager.messages[i].sender == PolybiusManager.userID) {
+                    message = Instantiate(blueMessage, messageParent.transform);
+                } else {
+                    message = Instantiate(greenMessage, messageParent.transform);
+                }
+                message.transform.position += new Vector3(0, -150f * PolybiusManager.messages.Count, 0);
+                message.transform.Find("Message").GetComponent<TextMeshPro>().text = PolybiusManager.messages[i].message;
+                message.transform.Find("Timestamp").GetComponent<TextMeshPro>().text = PolybiusManager.messages[i].time;
+                PolybiusManager.clearMessages();
+            }
+            */
+        }
+
+    }
+}
---
+++ Polybius/Assets/Scripts/UI/PolybiusManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace polybius {
+    public static class PolybiusManager {
+        // Enums
+        enum status { online, offline, invisible };
+
+        // Variables
+        public static string username;
+        public static int userID;
+        // public static List<ReplaceMe> messages = new List<ReplaceMe>();
+
+        // Example methods
+        // authenticate();
+        // establishDatabaseConnection();
+        // getUserStatus();
+
+        public static void getMessages() {
+            //TODO: update messages object
+        }
+
+        public static void clearMessages() {
+            //TODO: Empty messages list, update database if need be
+        }
+    }
+}
