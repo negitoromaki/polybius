dd66895f7daae819286565e7419c3a01b5013b01
nibarder <43069746+nibarder@users.noreply.github.com>
2018-09-29 22:56:56 -0400

    network
    working connection to server and database

--- Polybius/Assets/Scripts/Backend/Network/Connection.cs
+++ Polybius/Assets/Scripts/Backend/Network/Connection.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Sfs2X;
 using Sfs2X.Entities;
+using Sfs2X.Entities.Data;
 using Sfs2X.Core;
 using Sfs2X.Requests;
+
 
 public class Connection : MonoBehaviour {
 
 	public string ip = "";
 	public int port = 9933;
-	public static SmartFox sfs = new SmartFox();
+	public SmartFox sfs = new SmartFox();
+	public string initZone = "Polybius";
+	public bool logged = false;
 
 
 	// Use this for initialization
@@ -36,27 +40,57 @@ public class Connection : MonoBehaviour {
 	void onConnect(BaseEvent e){
 		if ((bool)e.Params ["success"]) {
 			Debug.Log ("Connected");
+			sfs.Send (new LoginRequest ("guest", "", initZone));
 
 		} else {
 			Debug.Log ("Connection failed");
 		}
 	}
 	void onLost(BaseEvent e){
-
+		logged = false;
 	}
 	void onLoggin(BaseEvent e){
+		logged = true;
 
 	}
 	void onResponse(BaseEvent e){
-
+		
+		string cmd = (string)e.Params ["cmd"];
+		ISFSObject paramsa = (SFSObject)e.Params["params"];
+		string message = cmd + " " + paramsa.GetUtfString("result") + " message: " + paramsa.GetUtfString ("message");
+		Debug.Log (cmd + " message: " + message);
 	}
 
 	//public methods
-	public void loggin(){
-
+	public void loggin(string username, string password){
+		ISFSObject l = new SFSObject();
+		l.PutUtfString ("username", username);
+		l.PutUtfString ("password", password);
+		sfs.Send (new ExtensionRequest ("UserLogin", l));
 	}
 
-	public void create(){
+	public void create(string username, string password, string email){
+		ISFSObject o = new SFSObject ();
+		o.PutUtfString ("username", username);
+		o.PutUtfString ("password", password);
+		o.PutUtfString ("email", email);
+		sfs.Send (new ExtensionRequest ("CreateUser", o));
+	}
+
+	public SmartFox getConnection(){
+		return sfs;
+	}
+	public bool isLogged(){
+		return logged;
+	}
+
+	//exit handler
+	void OnApplicationQuit(){
+		Debug.Log ("exiting");
+		sfs.RemoveAllEventListeners ();
+		if (sfs.IsConnected) {
+			sfs.Disconnect();
+		}
 
 	}
 
---
+++ Polybius/Assets/Scripts/Backend/Network/Tester.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Requests;
+using Sfs2X.Entities;
+using Sfs2X.Entities.Data;
+
+[RequireComponent(typeof(Connection))]
+public class Tester : MonoBehaviour {
+
+
+	// Use this for initialization
+	SmartFox sfs;
+	Connection connection;
+	void Start () {
+		connection = GetComponent<Connection> ();
+		sfs = connection.getConnection ();
+		StartCoroutine (tester ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator tester(){
+		while (!connection.isLogged()) {
+			yield return null;
+		}
+
+		Debug.Log ("Begin tests");
+
+		//test create
+		string username = "tim";
+		string password = "badpass";
+		string email = "tim@tim.com";
+
+		connection.create (username, password, email);
+		connection.loggin (username, password);
+
+
+	}
+
+
+
+}
