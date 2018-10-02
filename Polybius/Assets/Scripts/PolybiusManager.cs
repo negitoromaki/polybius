4bf30745e3becde344a1fea2cd6f71885979493c
Lucas <lucashahn@comcast.net>
2018-10-02 00:05:51 -0400

    Moved Connections to PolybiusManager

--- Polybius/Assets/Scripts/PolybiusManager.cs
+++ Polybius/Assets/Scripts/PolybiusManager.cs
@@ -6,9 +6,12 @@ using Sfs2X.Logging;
 using Sfs2X.Util;
 using Sfs2X.Core;
 using Sfs2X.Entities;
+using Sfs2X.Entities.Data;
+using Sfs2X.Requests;
 
 
-namespace polybius {
+namespace polybius
+{
     public class PolybiusManager : MonoBehaviour
     {
         // Enums
@@ -16,125 +19,144 @@ namespace polybius {
 
         // Variables
         public static User player;
-        public static Toggle debugT;
-        private static string host = "128.211.248.49";
-        private static string errormsg;
-        private static int port = 8080;
-        private SmartFox sfs;
+        public  string ip = "";
+        public  int port = 9933;
+        public  SmartFox sfs = new SmartFox();
+        public  string initZone = "Polybius";
+        public  bool logged = false;
 
+
+        // Use this for initialization
         void Start()
         {
-            player = new User();
+            sfs.AddEventListener(SFSEvent.LOGIN, onLoggin);
+            sfs.AddEventListener(SFSEvent.CONNECTION, onConnect);
+            sfs.AddEventListener(SFSEvent.CONNECTION_LOST, onLost);
+            sfs.AddEventListener(SFSEvent.EXTENSION_RESPONSE, onResponse);
+            sfs.ThreadSafeMode = true;
+
+            sfs.Connect(ip, port);
+
         }
 
+        // Update is called once per frame
         void Update()
         {
-            if (sfs != null)
+            sfs.ProcessEvents();
+
+        }
+
+
+        //event listeners
+        void onConnect(BaseEvent e)
+        {
+            if ((bool)e.Params["success"])
             {
-                sfs.ProcessEvents();
+                Debug.Log("Connected");
+                sfs.Send(new LoginRequest("guest", "", initZone));
+
+            }
+            else
+            {
+                Debug.Log("Connection failed");
             }
         }
+        void onLost(BaseEvent e)
+        {
+            logged = false;
+        }
+        void onLoggin(BaseEvent e)
+        {
+            logged = true;
+
+        }
+        void onResponse(BaseEvent e)
+        {
+
+            string cmd = (string)e.Params["cmd"];
+            ISFSObject paramsa = (SFSObject)e.Params["params"];
+            string message = cmd + " " + paramsa.GetUtfString("result") + " message: " + paramsa.GetUtfString("message");
+            Debug.Log(cmd + " message: " + message);
+        }
+
+        //public methods
+        public void login(string username, string password)
+        {
+            ISFSObject l = new SFSObject();
+            l.PutUtfString("username", username);
+            l.PutUtfString("password", password);
+            sfs.Send(new ExtensionRequest("UserLogin", l));
+        }
+
+        public void create(string username, string password, string email)
+        {
+            player = new User(username, password, email);
+            ISFSObject o = new SFSObject();
+            o.PutUtfString("username", username);
+            o.PutUtfString("password", password);
+            o.PutUtfString("email", email);
+            sfs.Send(new ExtensionRequest("CreateUser", o));
+        }
+
+        public SmartFox getConnection()
+        {
+            return sfs;
+        }
+        public bool isLogged()
+        {
+            return logged;
+        }
+
+        //exit handler
         void OnApplicationQuit()
         {
-            if (sfs != null && sfs.IsConnected)
-                sfs.Disconnect();
-        }
-        // Example methods
-        void authenticate()
-        {
-            
-            if (sfs == null || !sfs.IsConnected)
-            {
-                sfs = new SmartFox();
-
-                sfs.ThreadSafeMode = true;
-
-                sfs.AddEventListener(SFSEvent.CONNECTION, OnConnection);
-                sfs.AddEventListener(SFSEvent.CONNECTION_LOST, OnConnectionLost);
-                sfs.AddEventListener(SFSEvent.USER_VARIABLES_UPDATE, onUserVarsUpdate);
-
-
-                ConfigData cfg = new ConfigData();
-                cfg.Host = host;
-                cfg.Port = port;
-                cfg.Zone = "Polybius";
-                cfg.Debug = debugT.isOn;
-                sfs.Connect(cfg);
-            }
-            else
+            Debug.Log("exiting");
+            sfs.RemoveAllEventListeners();
+            if (sfs.IsConnected)
             {
                 sfs.Disconnect();
             }
 
         }
-        private void reset()
-        {
-            // Remove SFS2X listeners
-            sfs.RemoveEventListener(SFSEvent.CONNECTION, OnConnection);
-            sfs.RemoveEventListener(SFSEvent.CONNECTION_LOST, OnConnectionLost);
-            sfs.RemoveEventListener(SFSEvent.USER_VARIABLES_UPDATE, onUserVarsUpdate);
 
 
-            sfs = null;
-
-
-        }
-        private void OnConnectionLost(BaseEvent evt)
-        {
-            trace("Connection was lost; reason is: " + (string)evt.Params["reason"]);
-            reset();
-        }
-        
-        private void OnConnection(BaseEvent evt)
-        {
-            if ((bool)evt.Params["success"])
-            {
-                sfs.Send(new Sfs2X.Requests.LoginRequest(player.username, player.password, player.email));
-            }
-            else
-            {
-                reset();
-                errormsg = "Connection failed";
-            }
-        }
-        // establishDatabaseConnection();
-        // getUserStatus();
     }
-   
+        public class Message
+        {
+            public int sender;
+            public System.DateTime timestamp;
+            public string message;
+
+            public Message(int sender, System.DateTime timestamp, string message)
+            {
+                this.sender = sender;
+                this.timestamp = timestamp;
+                this.message = message;
+            }
+        }
+        public class User
+        {
+            public string username;
+            public int userID;
+            public string password;
+            public string email;
+            public List<Message> messages = new List<Message>();
+
+            public User()
+            {
+                username = null;
+                password = null;
+                email = null;
+                userID = -1;
+            }
+
+            public User(string u, string p, string e)
+            {
+                username = u;
+                password = p;
+                email = e;
+                userID = -1;
+            }
+        }
     
-
-    public class Message {
-        public int sender;
-        public System.DateTime timestamp;
-        public string message;
-
-        public Message(int sender, System.DateTime timestamp, string message) {
-            this.sender = sender;
-            this.timestamp = timestamp;
-            this.message = message;
-        }
-    }
-    public class User
-    { 
-        public string username;
-        public int userID;
-        public string password;
-        public string email;
-        public List<Message> messages = new List<Message>();
-
-        public User() {
-            username = null;
-            password = null;
-            email = null;
-            userID = -1;
-        }
-
-        public User(string u, string p, string e)
-        {
-            username = u;
-            password = p;
-            email = e;
-            userID = -1;
-        }
-    }
 }
