0f2299dc732042c7d0f0765cb2e2c428e21f69d8
tr-io <leoliu.mailbox@gmail.com>
2018-10-03 10:03:08 -0400

    Hooked up UI with server-side login/register

--- Polybius/Assets/Scripts/PolybiusManager.cs
+++ Polybius/Assets/Scripts/PolybiusManager.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Sfs2X;
 using Sfs2X.Logging;
 using Sfs2X.Util;
@@ -26,6 +27,11 @@ namespace polybius
         public SmartFox sfs = new SmartFox();
         public string initZone = "Polybius";
         public bool logged = false;
+
+        // gui handling
+        private string username = "";
+        private string password = "";
+        private string email = "";
 
         void Start()
         {
@@ -71,14 +77,41 @@ namespace polybius
 
         void onResponse(BaseEvent e)
         {
-
             string cmd = (string)e.Params["cmd"];
             ISFSObject paramsa = (SFSObject)e.Params["params"];
             string message = cmd + " " + paramsa.GetUtfString("result") + " message: " + paramsa.GetUtfString("message");
             Debug.Log(cmd + " message: " + message);
+
+            if (cmd == "UserLogin")
+            {
+                string result = paramsa.GetUtfString("result");
+                if (result == "success")
+                {
+                    // login successful
+                    Debug.Log("Login successful!");
+                }
+                else
+                {
+                    Debug.LogError("Error with registration: " + result);
+                }
+            }
+
+            if (cmd == "CreateUser")
+            {
+                string result = paramsa.GetUtfString("result");
+                if (result == "success")
+                {
+                    Debug.Log("Register successful!");
+                }
+                else
+                {
+                    Debug.LogError("Error with registration: " + result);
+                }
+            }
         }
 
         //public methods
+        // login user
         public void login(string username, string password)
         {
             ISFSObject l = new SFSObject();
@@ -87,6 +120,7 @@ namespace polybius
             sfs.Send(new ExtensionRequest("UserLogin", l));
         }
 
+        // register user
         public void create(string username, string password, string email, string dob)
         {
             player = new User(username, password, email,dob);
@@ -94,7 +128,7 @@ namespace polybius
             o.PutUtfString("username", username);
             o.PutUtfString("password", password);
             o.PutUtfString("email", email);
-            //o.PutUtfString("dob", dob);
+            //o.PutUtfString("dob", dob); // TODO: add DOB functionality
             sfs.Send(new ExtensionRequest("CreateUser", o));
         }
 
@@ -108,6 +142,58 @@ namespace polybius
             return logged;
         }
 
+
+        // UI handling for login/register
+        public void SetUsername(InputField _username)
+        {
+            if (!string.IsNullOrEmpty(_username.text))
+            {
+                username = _username.text;
+                Debug.Log("Username set to: " + username);
+            }
+               
+        }
+
+        public void SetPassword(InputField _password)
+        {
+            if (!string.IsNullOrEmpty(_password.text))
+            {
+                password = _password.text;
+                Debug.Log("Password set to: " + password);
+            }
+        }
+
+        public void SetEmail(InputField _email)
+        {
+            if (!string.IsNullOrEmpty(_email.text))
+            {
+                email = _email.text;
+                Debug.Log("Email set to: " + email);
+            }
+        }
+
+        // logging in
+        public void UILogin()
+        {
+            if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(username))
+            {
+                Debug.Log("Logging in user...");
+                login(username, password); // call login
+                password = ""; // wipe password
+            }
+        }
+
+        // register
+        public void UIRegister()
+        {
+            if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(email))
+            {
+                Debug.Log("Registering new user...");
+                create(username, password, email, ""); // call register (dob default nothing rn)
+                password = "";
+            }
+        }
+
         //exit handler
         void OnApplicationQuit()
         {
@@ -119,38 +205,39 @@ namespace polybius
             }
         }
     }
-        public class Message
-        {
-            public int sender;
-            public System.DateTime timestamp;
-            public string message;
 
-            public Message(int sender, System.DateTime timestamp, string message)
-            {
-                this.sender = sender;
-                this.timestamp = timestamp;
-                this.message = message;
-            }
+    public class Message
+    {
+        public int sender;
+        public System.DateTime timestamp;
+        public string message;
+
+        public Message(int sender, System.DateTime timestamp, string message)
+        {
+            this.sender = sender;
+            this.timestamp = timestamp;
+            this.message = message;
+        }
+    }
+
+    public class User
+    {
+        public string username, password, email, dob;
+        public int userID;
+        public List<Message> messages = new List<Message>();
+
+        public User() : this(null, null, null,null)
+        {
         }
 
-        public class User
+        public User(string u, string p, string e, string d)
         {
-            public string username, password, email, dob;
-            public int userID;
-            public List<Message> messages = new List<Message>();
-
-            public User() : this(null, null, null,null)
-            {
-            }
-
-            public User(string u, string p, string e, string d)
-            {
-                username = u;
-                password = p;
-                email = e;
-                dob = d;
-                userID = -1;
-            }
+            username = u;
+            password = p;
+            email = e;
+            dob = d;
+            userID = -1;
         }
+    }
     
 }
