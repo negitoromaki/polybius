20e32c400c614ab157a657a7a334cb97d04e8635
csculley <cs.sculley@gmail.com>
2018-10-21 20:54:02 -0400

    Redo all UI with new theme, add input filtering

--- Polybius/Assets/Scripts/UI/Panel Specific/MainMenuPanel.cs
+++ Polybius/Assets/Scripts/UI/Panel Specific/MainMenuPanel.cs
@@ -6,18 +6,5 @@ using TMPro;
 
 namespace polybius {
     public class MainMenuPanel : MonoBehaviour {
-        // This will be replaced later
-        public TextMeshProUGUI InfoPanelUsername, ProfilePanelUsername, usernameStat, dateOfBirthStat;
-
-        void Update() {
-            if (InfoPanelUsername.text      != PolybiusManager.player.username ||
-                ProfilePanelUsername.text   != PolybiusManager.player.username ||
-                usernameStat.text != PolybiusManager.player.username)
-
-                InfoPanelUsername.text = usernameStat.text = ProfilePanelUsername.text = PolybiusManager.player.username;
-
-            if (dateOfBirthStat.text != PolybiusManager.player.dob)
-                dateOfBirthStat.text = PolybiusManager.player.dob;
-        }
     }
 }
