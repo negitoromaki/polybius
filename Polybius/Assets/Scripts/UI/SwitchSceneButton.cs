b808390bdf52fd0becfd16fe590ddacdae94d036
csculley <cs.sculley@gmail.com>
2018-11-29 12:51:46 -0500

    Fix TicTacToe return to UI button

---
+++ Polybius/Assets/Scripts/UI/SwitchSceneButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneButton : MonoBehaviour {
+
+	public void goBackToUI() {
+        SceneManager.LoadScene("UI-Dev");
+    }
+}
